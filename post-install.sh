4a6d7ca4f53f1d4a081958c54afda910edbc67fc
dongepi <39761572+dongepi@users.noreply.github.com>
2024-03-25 13:22:53 +0200

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -6,36 +6,35 @@
 # Test to see if user is running with root privileges.
 if [[ "${UID}" -ne 0 ]]
 then
- echo 'Must execute with sudo or root' >&2
+ echo 'Must execute with or root' >&2
  exit 1
 fi
 
 # Ensure system is up to date
-sudo apt-get update -y 
+apt-get update -y 
 
 # Upgrade the system
-sudo apt-get upgrade -y
+apt-get upgrade -y
 
 # Install OpenSSH
-sudo apt-get install openssh-server -y
-
-# Enable Firewall
-sudo ufw enable 
-
-# configure the firewall
-sudo ufw allow OpenSSH
+apt-get install openssh-server -y
+apt-get install wget -y
 
 # Disabling root login 
 echo "PermitRootLogin no" >> /etc/ssh/sshd_config 
 echo "PermitEmptyPasswords no" /etc/ssh/sshd_config
 
+# Рестартиране на SSH
+systemctl restart ssh
+
+
 # Message of the day 
-sudo wget https://raw.githubusercontent.com/jwandrews99/Linux-Automation/master/misc/motd.sh
-sudo mv motd.sh /etc/update-motd.d/05-info
-sudo chmod +x /etc/update-motd.d/05-info
+wget https://raw.githubusercontent.com/jwandrews99/Linux-Automation/master/misc/motd.sh
+mv motd.sh /etc/update-motd.d/05-info
+chmod +x /etc/update-motd.d/05-info
 
 # Automatic downloads of security updates
-sudo apt-get install -y unattended-upgrades
+apt-get install -y unattended-upgrades
 echo "Unattended-Upgrade::Allowed-Origins {
 #   "${distro_id}:${distro_codename}-security";
 #//  "${distro_id}:${distro_codename}-updates";
@@ -45,33 +44,6 @@ echo "Unattended-Upgrade::Allowed-Origins {
 #Unattended-Upgrade::Automatic-Reboot "true"; 
 #}; " >> /etc/apt/apt.conf.d/50unattended-upgrades
 
-# Fail2Ban install 
-sudo apt-get install -y fail2ban
-sudo systemctl start fail2ban
-sudo systemctl enable fail2ban
-
-echo "
-[sshd]
-enabled = true
-port = 22
-filter = sshd
-logpath = /var/log/auth.log
-maxretry = 4
-" >> /etc/fail2ban/jail.local
-
-# SpeedTest Install
-sudo apt-get install speedtest-cli -y
-
-# SFTP Server / FTP server that runs over ssh
-echo "
-Match group sftp
-ChrootDirectory /home
-X11Forwarding no
-AllowTcpForwarding no
-ForceCommand internal-sftp
-" >> /etc/ssh/sshd_config
-
-sudo service ssh restart  
 
 # Docker option install 
 echo "
@@ -81,16 +53,16 @@ Do you want to install docker? If so type y / If you dont want to install enter 
 
 ######################################################################################################
 "
-read $docker
+read docker
 
-if [[ $docker -eq "y" ]] || [[ $docker -eq "yes" ]]; then
-    sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-    sudo apt-get update -y
+if [[ "$docker" == "y" ]] || [[ "$docker" == "yes" ]]; then
+    apt install apt-transport-https ca-certificates curl software-properties-common -y
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+    apt-get update -y
     apt-cache policy docker-ce
-    sudo apt install docker-ce -y
-    sudo apt-get install docker-compose -y 
+    apt install docker-ce -y
+    apt-get install docker-compose -y 
 
     echo " 
     
@@ -98,10 +70,16 @@ if [[ $docker -eq "y" ]] || [[ $docker -eq "yes" ]]; then
 
     "
 
-    sudo docker volume create portainer_data
-    sudo docker run -d -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
+    docker volume create portainer_data
+    docker run -d -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
 
     echo "
+elif [[ "$docker" == "n" ]] || [[ "$docker" == "no" ]]; then
+    echo "Skipping Docker installation."
+else
+    echo "Invalid input. Please enter 'y' or 'n'."
+fi
+
 #####################################################################################################    
                             Congrats Docker has been installed
 ######################################################################################################
@@ -135,8 +113,8 @@ else
 fi
 
 # Cleanup
-sudo apt autoremove
-sudo apt clean 
+apt autoremove
+apt clean 
 
 echo "
 ######################################################################################################
